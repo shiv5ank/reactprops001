4bd5494b5d232744fea09cc2f404d96a06bd361c
shiv5ank <Shivank0596@gmail.com>
2022-11-15 17:40:32 +0530

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,69 @@
+const Cards = () => {
+  return (
+    <div className="main_container">
+      <div className="card_container">
+        <div className="cards">
+          <p className="pink"></p>
+          <p class="text">#ff6663</p>
+          <p class="text_pink">Pink</p>
+        </div>
+
+        <div className="cards">
+          <p className="grey"></p>
+          <p class="text">#333333</p>
+          <p class="text_grey">Grey</p>
+        </div>
+
+        <div className="cards">
+          <p className="black"></p>
+          <p class="text">#000000</p>
+          <p class="text_black">Black</p>
+        </div>
+
+        <div className="cards">
+          <p className="green"></p>
+          <p class="text">#38bb14</p>
+          <p class="text_green">Green</p>
+        </div>
+
+        <div className="cards">
+          <p className="red"></p>
+          <p class="text">#c90b0b</p>
+          <p class="text_red">Red</p>
+        </div>
+
+        <div className="cards">
+          <p className="orange"></p>
+          <p class="text">#ff8000</p>
+          <p class="text_orange">Orange</p>
+        </div>
+
+        <div className="cards">
+          <p className="yellow"></p>
+          <p class="text">#fff500</p>
+          <p class="text_yellow">Yellow</p>
+        </div>
+
+        <div className="cards">
+          <p className="light_grey"></p>
+          <p class="text">#cccccc</p>
+          <p class="text_light_grey">Light Grey</p>
+        </div>
+
+        <div className="cards">
+          <p className="purple"></p>
+          <p class="text">#7e41a2</p>
+          <p class="text_purple">Purple</p>
+        </div>
+
+        <div className="cards">
+          <p className="brown"></p>
+          <p class="text">#c14911</p>
+          <p class="text_brown">Brown</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
